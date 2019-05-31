aeeb24b6d6a750f09e3207fec5ff3863a6e1ddc2
Javier Antón Moral <javier.anton@juanxxiii.net>
2019-05-31 12:08:01 +0200

    PRIMER CAMBIO PRUEBA 2 COMPARTIDA

---
+++ WINGITPRUEBA2/WINGITPRUEBA2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WINGITPRUEBA2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ESTOY EN PRUEBA 2 COMPARTIDA");
+        }
+    }
+}
